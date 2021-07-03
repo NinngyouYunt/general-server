72788988fd3b22b6edf5adbedaec22b12d9f6465
Yunt <PeterLiangAP@Outlook.com>
2021-07-03 04:40:00 -0400

    update port

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const config = {
-  port: 3000 || process.env.PORT,
+  port: process.env.PORT,
   mongo: {
     username: process.env.mongoUsername,
     password: process.env.mongoPassword
